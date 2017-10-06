ce71e5447b8130eb9c5458aaa41b0ee767d1b6f5
Davie Clarkson <clarkee013@gmail.com>
2017-10-06 15:07:25 +0100

    indentation housekeeping

--- client/db/StatusQuery.js
+++ client/db/StatusQuery.js
@@ -1,46 +1,48 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var StatusQuery = function(){
-  this.url = "mongodb://localhost:27017/status_checker";
+var StatusQuery = function () {
+    this.url = "mongodb://localhost:27017/status_checker";
 };
 
 StatusQuery.prototype = {
-    all: function(callback){
-        MongoClient.connect(this.url, function(err, db){
-          var collection = db.collection('status');
-          collection.find().toArray(function(err, result){
-            callback(result);
-          });
+    all: function (callback) {
+        MongoClient.connect(this.url, function (err, db) {
+            var collection = db.collection('status');
+            collection.find().toArray(function (err, result) {
+                callback(result);
+            });
         });
-      },
+    },
 
-      add: function(statusToAdd, onQueryFinished){
-          MongoClient.connect(this.url, function(err, db){
-              if(db){
-                  var collection = db.collection('status');
-                  collection.insert(statusToAdd);
-                  collection.find().toArray(function(err, docs){
-                      onQueryFinished(docs);
-                  });
-              };
-          });
-      },
+    add: function (statusToAdd, onQueryFinished) {
+        MongoClient.connect(this.url, function (err, db) {
+            if (db) {
+                var collection = db.collection('status');
+                collection.insert(statusToAdd);
+                collection.find().toArray(function (err, docs) {
+                    onQueryFinished(docs);
+                });
+            };
+        });
+    },
 
-      findById: function(id, callback){
-        MongoClient.connect(this.url, function(err, db){
-         var collection = db.collection('status');
-         collection.findOne({"_id": new ObjectId(id)}, function(err, result) {
-          callback(result);
-              });
-       });
-     },     
-
-
-
-
-     
-     
-   };
+    findById: function (id, callback) {
+        MongoClient.connect(this.url, function (err, db) {
+            var collection = db.collection('status');
+            collection.findOne({
+                "_id": new ObjectId(id)
+            }, function (err, result) {
+                callback(result);
+            });
+        });
+    },
 
 
-    module.exports = StatusQuery
+
+
+
+
+};
+
+
+module.exports = StatusQuery
