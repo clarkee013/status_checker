725d130c89229a7416897865985c33d882b9e98e
Davie Clarkson <clarkee013@gmail.com>
2017-10-06 14:34:05 +0100

    delete DB functions started

--- client/db/commentQuery.js
+++ client/db/commentQuery.js
@@ -27,16 +27,34 @@ CommentQuery.prototype = {
         });
     },
 
-    findById: function(id, callback){
+    findCommentByCommentId: function(id, callback){
         MongoClient.connect(this.url, function(err, db){
          var collection = db.collection('comments');
-         collection.findOne({"_id": new ObjectId(id)}, function(err, result) {
+         collection.findOne({"_id": new ObjectId(id)}).toArray(function(err, result) {
           callback(result);
               });
        });
-    },     
-
+    }, 
     
+    allCommentsByEventId: function(eventId, callback){
+        MongoClient.connect(this.url, function(err, db){
+            var collection = db.collection('comments');
+            collection.find({}, {"eventId" : new Object(eventId)}).toArray(function(err, result){
+                callback(result);
+            });
+        });
+    },
+
+    deleteCommentByCommentId: function (commentId, callback) {
+        MongoClient.connect(this.url, function (err, db) {
+            var collection = db.collection('comments');
+            collection.deleteOne({"_id": new ObjectId(commentId)}, function (err, result) {
+                callback(result);
+            });
+        });
+    },
+
+
      
      
 };
--- client/db/eventQuery.js
+++ client/db/eventQuery.js
@@ -1,39 +1,63 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var EventQuery = function(){
-  this.url = "mongodb://localhost:27017/status_checker";
+var EventQuery = function () {
+    this.url = "mongodb://localhost:27017/status_checker";
 };
 
 EventQuery.prototype = {
-      all: function(callback){
-    MongoClient.connect(this.url, function(err, db){
-      var collection = db.collection('events');
-      collection.find().toArray(function(err, result){
-        callback(result);
-      });
-    });
-  },
-    add: function(commentToAdd, onQueryFinished){
-        MongoClient.connect(this.url, function(err, db){
-            if(db){
+
+    all: function (callback) {
+        MongoClient.connect(this.url, function (err, db) {
+            var collection = db.collection('events');
+            collection.find().toArray(function (err, result) {
+                callback(result);
+            });
+        });
+    },
+
+    add: function (commentToAdd, onQueryFinished) {
+        MongoClient.connect(this.url, function (err, db) {
+            if (db) {
                 var collection = db.collection('events');
                 collection.insert(commentToAdd);
-                collection.find().toArray(function(err, docs){
+                collection.find().toArray(function (err, docs) {
                     onQueryFinished(docs);
                 });
             };
         });
     },
-    findById: function(id, callback){
-        MongoClient.connect(this.url, function(err, db){
-         var collection = db.collection('events');
-         collection.findOne({"_id": new ObjectId(id)}, function(err, result) {
-          callback(result);
-              });
-       });
-     },     
-     
-     
-   };
 
-module.exports = EventQuery; 
+    findEventByEventId: function (eventId, callback) {
+        MongoClient.connect(this.url, function (err, db) {
+            var collection = db.collection('events');
+            collection.findOne({"_id": new ObjectId(eventId)}).toArray(function (err, result) {
+                callback(result);
+            });
+        });
+    },
+
+    allEventsByStatusId: function(statusId, callback) {
+        MongoClient.connect(this.url, function(err, db){
+            var collection = db.collection('events');
+            collection.find({}, {"statusId" : new Object(statusId)}).toArray(function(err, result){
+                callback(result);
+            });
+        });
+    },
+
+    deleteEventByEventId: function (eventId, callback) {
+        MongoClient.connect(this.url, function (err, db) {
+            var collection = db.collection('events');
+            collection.deleteOne({"_id": new ObjectId(eventId)}, function (err, result) {
+                callback(result);
+            });
+        });
+    },
+    // dont think i need the callback for a delete - would it be a onQueryFinished like the all?
+
+
+
+
+};
+
+module.exports = EventQuery;
